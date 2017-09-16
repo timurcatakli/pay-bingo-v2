48bb450e0aa4f3dbbc4118c99037f4cf43cd8c54
Timur Catakli <tcatakli@demandbase.com>
2017-09-16 02:37:44 -0700

    Install configure react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import { Row, Button, ButtonToolbar, Col, Panel } from 'react-bootstrap'
 const propTypes = {}
 
 class App extends React.Component {
   render() {
     return (
-      <p>App class component is working</p>
+      <ButtonToolbar>
+        {/* Standard button */}
+        <Button>Default</Button>
+
+        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+        <Button bsStyle="primary">Primary</Button>
+
+        {/* Indicates a successful or positive action */}
+        <Button bsStyle="success">Success</Button>
+
+        {/* Contextual button for informational alert messages */}
+        <Button bsStyle="info">Info</Button>
+
+        {/* Indicates caution should be taken with this action */}
+        <Button bsStyle="warning">Warning</Button>
+
+        {/* Indicates a dangerous or potentially negative action */}
+        <Button bsStyle="danger">Danger</Button>
+
+        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+        <Button bsStyle="link">Link</Button>
+      </ButtonToolbar>
     )
   }
 }
