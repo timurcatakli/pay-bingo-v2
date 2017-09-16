0bb281cc48a9d624427e20b5bd74edebd1e7c8b3
Timur Catakli <tcatakli@demandbase.com>
2017-09-16 01:58:19 -0700

    Complete css loader config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ module.exports = {
           presets: ['es2015', 'react'],
           plugins: ['transform-class-properties']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
       }
     ]
   }
